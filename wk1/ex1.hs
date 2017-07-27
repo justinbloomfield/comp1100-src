5a4f9fe8629c82ea660b1540a8d087efc8883340
Justin Bloomfield <u6053932@n113lt16.csitlabs>
2017-07-27 15:24:35 +1000

    Add comments because was asked to do so

--- wk1/ex1.hs
+++ wk1/ex1.hs
@@ -1,9 +1,12 @@
+-- add 1 to an integer
 addOne :: Integer -> Integer
 addOne x = x + 1
 
+-- calculate area of square/rectangle using floats
 rectArea :: Float -> Float -> Float
 rectArea l w = l * w
 
+-- area thingy ()
 pythagArea :: Float -> Float -> Float -> Float
 pythagArea a b c =
     let s = (a + b + c) / 2
